316ac73afa2f73415b624609558a40ed3d425466
Sebastian E Cornejo B <secornejob@tottus.cl>
2020-04-03 20:51:22 -0300

    Desarrollo Modelo Proveedor
    -> Se configura app en la raiz del proyecto.
    -> Se agrega el modelo al sitio de administracion de Django.
    -> Se desarrolla un  modelo base para manejar las marcas de tiempo.
    -> Se desarrollo modelo para manejar la maestra de proveedores.
    -> Se generan las migraciones a las bases de datos.

--- proveedores/models.py
+++ proveedores/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class TimestampModel(models.Model):
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+
+	class Meta:
+		abstract = True	
+
+
+class Proveedor(TimestampModel):
+	id = models.IntegerField(primary_key = True)
+	codigo = models.IntegerField(unique = True)
+	rut = models.IntegerField(unique = True)
+	razon_social = models.CharField(max_length = 50)
+
+	class Meta:
+		verbose_name_plural = 'Proveedores'
+		ordering = ['razon_social']
+	
+	def __str__(self):
+		return '%s'% (self.razon_social)
