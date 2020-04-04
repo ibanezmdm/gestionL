ec16065c024fc588daae62a52f62712b559792e0
Sebastian E Cornejo B <secornejob@tottus.cl>
2020-04-04 00:31:12 -0300

    App Colaboracion: Se generan modelos
    Se generan 2 modelos con los cuales se manejara esta aplicacion
    
    Se generan 2 modelos con los cuales se manejara esta aplicacion
    1.- Colaborador: correponde a los proveedores que se encuentran dentro
    del programa de colaboracion.
    2.- ContactoProveedor: Base de correos de proveedores, que se enlaza al
    modelo Colaborador a traves de una relacion de tipo ManyToMany

--- GestionDesarrollo/settings.py
+++ GestionDesarrollo/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'proveedores.apps.ProveedoresConfig'
+    'proveedores.apps.ProveedoresConfig',
+    'colaboracion.apps.ColaboracionConfig',
 ]
 
 MIDDLEWARE = [
--- colaboracion/models.py
+++ colaboracion/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from proveedores.models import Proveedor, TimestampModel
+
 
 # Create your models here.
+
+class Colaborador(TimestampModel):
+	proveedor = models.OneToOneField(Proveedor, on_delete = models.PROTECT, primary_key = True)
+	estado = models.BooleanField(default = True)
+
+	class Meta:
+		verbose_name_plural = 'Proveedores Colaboración'
+		ordering = ['proveedor']
+
+	def __str__(self):
+		return '%s'% (self.proveedor)
+
+
+
+class ContactoProveedor(TimestampModel):
+	proveedor = models.ManyToManyField(Colaborador)
+	nombre = models.CharField(max_length = 50, null = True, blank = True)
+	apellido = models.CharField(max_length = 50, null = True, blank = True)
+	email = models.EmailField(primary_key = True)
+	en_copia = models.BooleanField(default = False)
+
+	class Meta:
+		verbose_name_plural = 'Contactos Proveedores'
+		ordering = ['email']
+	
+	def __str__(self):
+		return '%s'% (self.email)
